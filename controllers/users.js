ffef5c26e79bd09416a2e4f2bfe030de7827f075
mihail kiyamov <mihailkyamov@yandex.ru>
2023-08-08 12:51:27 +0500

    diplom-2

--- controllers/users.js
+++ controllers/users.js
@@ -70,7 +70,9 @@ module.exports.patchUser = (req, res, next) => {
   User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.code === 11000) {
+        next(new DublicateError('Пользователь с такой почтой уже существует'));
+      } else if (err.name === 'ValidationError') {
         next(new WrongDataError('Некорректные данные'));
       } else if (err.name === 'CastError') {
         next(new WrongDataError('Некорректные данные'));
